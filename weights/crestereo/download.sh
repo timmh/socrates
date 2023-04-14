04ca18ba5a9d04ae122e46f9e5dad17c59a2b69e
Timm Haucke <timm@haucke.xyz>
2023-04-14 15:52:33 +0200

    update weight location

--- weights/crestereo/download.sh
+++ weights/crestereo/download.sh
@@ -1,29 +1,5 @@
 #!/bin/bash
 
-# adapted from https://github.com/PINTO0309/PINTO_model_zoo/tree/aaf732fbd4c9a15d2b1147694725dfa39a18d239/284_CREStereo
-
-fileid="1g4TJafSuIkqVEjZ71Duhwyog3BdTg6zx"
-html=`curl -c ./cookie -s -L "https://drive.google.com/uc?export=download&id=${fileid}"`
-curl -Lb ./cookie "https://drive.google.com/uc?export=download&`echo ${html}|grep -Po '(confirm=[a-zA-Z0-9\-_]+)'`&id=${fileid}" -o resources.tar.gz
-tar -zxvf resources.tar.gz
-rm resources.tar.gz
-
-fileid="13kpCV_ANTsfNHNWTxf8JFBoQnRIbcRKD"
-html=`curl -c ./cookie -s -L "https://drive.google.com/uc?export=download&id=${fileid}"`
-curl -Lb ./cookie "https://drive.google.com/uc?export=download&`echo ${html}|grep -Po '(confirm=[a-zA-Z0-9\-_]+)'`&id=${fileid}" -o resources.tar.gz
-tar -zxvf resources.tar.gz
-rm resources.tar.gz
-
-fileid="1XM88ZREfBnv4u1K4yYC2wS52HBmp-17J"
-html=`curl -c ./cookie -s -L "https://drive.google.com/uc?export=download&id=${fileid}"`
-curl -Lb ./cookie "https://drive.google.com/uc?export=download&`echo ${html}|grep -Po '(confirm=[a-zA-Z0-9\-_]+)'`&id=${fileid}" -o resources.tar.gz
-tar -zxvf resources.tar.gz
-rm resources.tar.gz
-
-fileid="1uBkG5_1TDSE4MqxbqcUASWKoLaV-nE4F"
-html=`curl -c ./cookie -s -L "https://drive.google.com/uc?export=download&id=${fileid}"`
-curl -Lb ./cookie "https://drive.google.com/uc?export=download&`echo ${html}|grep -Po '(confirm=[a-zA-Z0-9\-_]+)'`&id=${fileid}" -o resources.tar.gz
-tar -zxvf resources.tar.gz
-rm resources.tar.gz
+wget --content-disposition -P "$(dirname $0)" https://github.com/timmh/ONNX-CREStereo-Depth-Estimation/releases/download/v1.0.0/crestereo_combined_iter5_720x1280.onnx
 
 echo Download finished.
